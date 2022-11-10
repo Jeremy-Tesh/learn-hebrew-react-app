cebb0bc0887a9ee13d12278205d1c668e96083f3
jeremy-tesh <jermitesh20@gmail.com>
2022-11-10 15:33:48 +0300

    responsiveness and minor ui change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Header() {
   return (
-    <div>
-      <Navbar className="bg-[#0073aa] font-Koulen text-xl" variant="dark">
-        <Container className="flex justify-between  w-full  ">
-          <Navbar.Brand href="/" className="mx-7">
-            Hooked on Hebrew
-          </Navbar.Brand>
-          <Nav className="me-auto mx-6 ">
+    <Navbar
+      className="bg-[#0073aa] font-Koulen text-xl"
+      variant="dark"
+      expand="lg"
+    >
+      <Container fluid className="flex justify-between  w-full  ">
+        <Navbar.Brand href="/" className="mx-7">
+          Hooked on Hebrew
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
             <Nav.Link className="mx-3" href="/">
               Home
             </Nav.Link>
@@ -20,9 +32,9 @@ function Header() {
               About us
             </Nav.Link>
           </Nav>
-        </Container>
-      </Navbar>
-    </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,53 +2,47 @@ import React from "react";
 import Img1 from "../assets/images/card1.png";
 function Home() {
   return (
-    <div className="w-screen h-screen ">
-      <div className="relative">
-        <div
-          className="h-[500px] bg-cover object-cover w-screen"
-          style={{
-            backgroundImage:
-              "url('http://cdn.designbeep.com/wp-content/uploads/2019/08/featured-image-31.jpg')",
-            backgroundPosition: "cover",
-          }}
-        >
-          {/* <img
+    <div className="w-full h-auto ">
+      <div
+        className="h-[500px] bg-cover object-cover w-full"
+        style={{
+          backgroundImage:
+            "url('http://cdn.designbeep.com/wp-content/uploads/2019/08/featured-image-31.jpg')",
+          backgroundPosition: "cover",
+        }}
+      >
+        {/* <img
             className="h-[500px] bg-gradient-to-b bg-transparent bg-['#837960'] object-cover w-full"
             src="http://cdn.designbeep.com/wp-content/uploads/2019/08/featured-image-31.jpg"
             alt="img"
           /> */}
-          <div className="w-screen relative top-20 text-center">
-            <h1 className="r text-white font-Koulen text-9xl">
-              Hooked on Hebrew{" "}
-            </h1>
-          </div>
+        <div className="w-screen relative top-20 text-center">
+          <h1 className="r text-white font-Koulen text-9xl">
+            Hooked on Hebrew{" "}
+          </h1>
         </div>
+      </div>
 
-        <div className="flex p-5">
-          <div>
-            <h1 className="font-Koulen">
-              Shalom and Welcome to Hooked on Hebrew!
-            </h1>
-            <br></br>
+      <div className="flex p-5">
+        <div>
+          <h1 className="font-Koulen">
+            Shalom and Welcome to Hooked on Hebrew!
+          </h1>
+          <br></br>
 
-            <p className="font-Comfortaa">
-              Here you will find creative learning aids to develop a greater
-              proficiency level in reading biblical Hebrew. Most of the Bible
-              was written in Hebrew (77.3%) and that should motivate you to
-              learn this language. Though you may think learning biblical Hebrew
-              is complex, this site's resources might help you ace your
-              understanding and have a smooth safari on the rough road of this
-              ancient language. We hope you will be hooked to Hebrew and talk
-              about it when you sit at home and when you walk along the road,
-              when you lie down and when you get up. Blessings,
-            </p>
-          </div>
-          <img
-            className="relative h-[400px] w-[400px] m-4"
-            src={Img1}
-            alt="img"
-          />
+          <p className="font-Comfortaa">
+            Here you will find creative learning aids to develop a greater
+            proficiency level in reading biblical Hebrew. Most of the Bible was
+            written in Hebrew (77.3%) and that should motivate you to learn this
+            language. Though you may think learning biblical Hebrew is complex,
+            this site's resources might help you ace your understanding and have
+            a smooth safari on the rough road of this ancient language. We hope
+            you will be hooked to Hebrew and talk about it when you sit at home
+            and when you walk along the road, when you lie down and when you get
+            up. Blessings,
+          </p>
         </div>
+        <img className="w-[20%] h-[20%]" src={Img1} alt="img" />
       </div>
     </div>
   );
