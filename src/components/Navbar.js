25380fdbbf79623c241cc58cbd9b340fd61302db
jeremy-tesh <jermitesh20@gmail.com>
2022-11-10 15:39:02 +0300

    remove unused imports

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
