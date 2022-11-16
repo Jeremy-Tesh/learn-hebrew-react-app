4d4a42a195b61486c5db01928fc5c216af4e64d8
jenprealubit <jenniferprealubit@gmail.com>
2022-11-16 23:59:28 +0800

    resources page and home page image

---
+++ src/data/ResourceImages.js
@@ -0,0 +1,72 @@
+
+export const resourceImages = [
+    {
+        img: require('../assets/file/ets_presentation/1.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/2.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/3.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/4.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/5.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/6.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/7.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/8.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/9.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/10.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/11.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/12.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/13.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/14.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/15.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/16.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/17.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/18.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/19.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/20.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/21.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/22.jpg'),
+    },
+    {
+        img: require('../assets/file/ets_presentation/23.jpg'),
+    },
+]
